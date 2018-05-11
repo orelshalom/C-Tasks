3eb99aef33c68efb5f374e2fd081ef821381425b
Orel Shalom <33144903+orelshalom@users.noreply.github.com>
2018-05-11 09:37:20 +0300

    Delete a.h

--- GNIM Game/a.h
+++
@@ -1,4 +0,0 @@
-void Order (int *x, int *y, int *z);
-void UserChoice(int * a, int * b, int * x, int * y, int * z);
-int IsWinner(int * x, int * y, int * z, int * loser);
-int IsLoser(int * x, int * y, int * z, int * loser);
